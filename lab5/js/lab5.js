7247def0dc711a6e16adaa1be22f99a43f690112
William R <68865692+ramoswilliam@users.noreply.github.com>
2020-09-16 12:19:27 -0700

    Create lab5.js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/*
+ * Author: William Ramos <wramos@csumb.edu>
+ * Created: 16 September 2020
+ * License: Public Domain
+ */
+
+// Define Variables
+carMake = "Toyota";
+carModel; = "Corolla";
+carColor; = "Gray";
+carYear = "2010";
+year = "2020"
+
+//calculate
+Age = year - carYear;
+
+//output
+document.writeIn("Make: " + carMake + "<br>");
+document.writeIn("Model: " + carModel + "<br>");
+document.writeIn("carColor: " + "<br>");
+document.writeIn("carYear: " + "<br>");
+document.writeIn("Age: " + age + " year(s)<br>");
