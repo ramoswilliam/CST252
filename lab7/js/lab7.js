8bdea4f9fb464a6181afa5c6a84cb1308067e090
William R <68865692+ramoswilliam@users.noreply.github.com>
2020-09-23 11:20:06 -0700

    Update lab7.js

---
+++ lab7/js/lab7.js
@@ -0,0 +1,12 @@
+/*
+ * Author: William Ramos <wramos@csumb.edu>
+ * Created: 23 September 2020
+ * License: Public Domain
+ */
+
+// Functions
+
+function sortUserName() {
+	var userName = window.prompt("State your name");
+	console.log("userName =", userName);
+}
