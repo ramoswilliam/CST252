b929c18c0e00847c30a6b31218d15ee7caeef400
William R <68865692+ramoswilliam@users.noreply.github.com>
2020-10-03 16:32:02 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -1,30 +1,8 @@
 /*
  * Author: William Ramos <wramos@csumb.edu>
- * Created: 23 September 2020
+ * Created: 3 October 2020
  * License: Public Domain
  */
 
 // Functions
 
-function sortUserName() {
-	var userName = window.prompt("State your name");
-	console.log("userName = ", userName);
-
-// split string to array
-var nameArray = userName,split('');
-console.log("nameArray =", namArray);
-
-//sort array
-var nameArraySort = nameArray.sort();
-console.log("nameArraySort =", nameArraySort);
-
-//join array back to a string
-var nameSorted = nameArraySort.join('');
-console.log("nameSorted =", nameSorted);
-
-return nameSorted;
-}
-
-// output
-document.writeln("Your new name: ",
-	sortUserName(), "</br>");
